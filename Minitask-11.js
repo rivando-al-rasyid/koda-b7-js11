e60ccdae2acc67e387e960ac75f43cfbd5e29940
Rivando Al rasyid <119521535+rivando-al-rasyid@users.noreply.github.com>
2026-02-27 21:58:42 +0700

    Add files via upload

--- Minitask-11.js
+++ Minitask-11.js
@@ -1,14 +1,36 @@
-let pangilNama = (nama) => {
+let perkenalan = function(delay , value){
+  return new Promise((resolve) => {
+        setTimeout(() => {
+        console.log(value);
+        resolve(value);
+    }, delay);
 
-new Promise(function (resolve, reject) {
-  setTimeout(pangilNama,1500,"john");
-})
-  .then(function (nama) {
-  setTimeout(pangilNama,500,"jane")
-})
-  .then(function (nama) {
-  setTimeout(pangilNama,500,"jane")
   })
-);
-  console.log(nama)
+}
+perkenalan(1500,"jhon").then(() => perkenalan(2000, "ed"))
+  .then(() => perkenalan(500, "jane"));
+
+(async function () {
+  await perkenalan(1500,"jhon")
+    await perkenalan(2000,"ed")
+  await perkenalan(1500,"jane")
+
+})()
+
+
+// Source - https://stackoverflow.com/a/22707551
+// Posted by T.J. Crowder, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-02-27, License - CC BY-SA 3.0
+
+function later(delay, value) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, delay, value); // Note the order, `delay` before `value`
+        /* Or for outdated browsers that don't support doing that:
+        setTimeout(function() {
+            resolve(value);
+        }, delay);
+        Or alternately:
+        setTimeout(resolve.bind(null, value), delay);
+        */
+    });
 }
