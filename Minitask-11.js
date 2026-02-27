de61469f4c7c6a8e9ee18471b78fef822125073e
Rivando Al rasyid <119521535+rivando-al-rasyid@users.noreply.github.com>
2026-02-27 15:40:35 +0700

    Add files via upload

--- Minitask-11.js
+++ Minitask-11.js
@@ -1,18 +1,14 @@
 let pangilNama = (nama) => {
-  console.log(nama)
-}
-
-
-setTimeout(pangilNama,1500,"john")
-setTimeout(pangilNama,500,"jane")
 
 new Promise(function (resolve, reject) {
   setTimeout(pangilNama,1500,"john");
 })
-  .then(function (result) {
+  .then(function (nama) {
   setTimeout(pangilNama,500,"jane")
 })
-  .then(function (result) {
+  .then(function (nama) {
   setTimeout(pangilNama,500,"jane")
   })
 );
+  console.log(nama)
+}
